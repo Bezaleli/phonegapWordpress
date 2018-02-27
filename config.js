fe469493feca85585a5de0dee8f87029cfbe593f
John <j.mtulya123@gmail.com>
2018-02-27 20:15:33 +0300

    Add All Files

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'afrisoka',
+		wp_ws_url : 'http://afrisoka.com/wp-appkit-api/afrisoka',
+		wp_url : 'http://afrisoka.com',
+		theme : 'q-android',
+		version : '4.5',
+		app_title : 'Afrisoka',
+		app_platform : 'android',
+		gmt_offset : 0,
+		debug_mode : 'on',
+		auth_key : '!KMt,Hqe+;or?:A/+%Y#EyKh[&5lqt[&kvf=nwv{{#KiG%lxzzlR-MLfn4!8:h9:',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
